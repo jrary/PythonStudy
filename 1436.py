45b094de2a54681d80f53293b37c0ec228a4cbd4
jrary <jrary@naver.com>
2023-01-18 17:56:15 +0900

    23/01/18 = #1436

---
+++ 1436.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+i = 0
+res = 0
+while n != res:
+    i = i+1
+    temp = i
+    while temp >= 666:
+        if temp % 1000 == 666:
+            res = res+1
+            break
+        else:
+            temp = temp//10
+
+print(i)
