160ac20c4acc935cfcf969fc564a3d4e7e72444c
jrary <jrary@naver.com>
2023-02-27 00:05:35 +0900

    23/02/26 - #1712

--- 1712.py
+++ 1712.py
@@ -1,6 +1,6 @@
 import sys
 a, b, c = map(int, sys.stdin.readline().split())
-res = -1
-while (a+b*res) >= c*res:
-    res += 1
-print(res)
+if b >= c:
+    print(-1)
+else:
+    print(int(a/(c-b)+1))
