5bb4b0564bff45651a958c1378769b277bb5fb30
jrary <jrary@naver.com>
2023-03-03 11:03:00 +0900

    23/03/03 - #2231

---
+++ 2231.py
@@ -0,0 +1,14 @@
+import sys
+n = int(sys.stdin.readline())
+res = 0
+for i in range(n):
+    m = i
+    temp = i
+    while temp != 0:
+        m += temp % 10
+        temp //= 10
+    if m == n:
+        print(m)
+        res = i
+        break
+print(res)
