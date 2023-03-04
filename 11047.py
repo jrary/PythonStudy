ec2494139b4965ec200cb5e44f2b2cfc845dad97
jrary <jrary@naver.com>
2023-03-05 00:30:21 +0900

    23/03/05

---
+++ 11047.py
@@ -0,0 +1,12 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+coin = []
+for _ in range(n):
+    coin.append(int(sys.stdin.readline()))
+res = 0
+coin.sort(reverse=True)
+for i in coin:
+    while k >= i:
+        k -= i
+        res += 1
+print(res)
