9251cf16d9bcd2da9b3825f2b4370e980a335a6f
jrary <jrary@naver.com>
2023-03-31 04:35:41 +0900

    23/03/31 - #7785

---
+++ 7785.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline())
+company = []
+for _ in range(n):
+    a, b = map(str, sys.stdin.readline().split())
+    if b == 'enter':
+        company.append(a)
+    elif b == 'leave':
+        company.remove(a)
+
+company.sort(reverse=True)
+for i in company:
+    print(i)
