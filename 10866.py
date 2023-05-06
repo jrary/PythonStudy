79a5f847db71358439ac8c3f6826e0b870814512
jrary <jrary@naver.com>
2023-05-06 23:59:15 +0900

    23/05/06 - #10866

---
+++ 10866.py
@@ -0,0 +1,42 @@
+from collections import deque
+import sys
+N = int(sys.stdin.readline())
+q = deque()
+for _ in range(N):
+    line = sys.stdin.readline().strip()
+    try:
+        act, i = map(str, line.split())
+        i = int(i)
+    except:
+        act = line
+    if act == 'push_front':
+        q.extendleft(i)
+    elif act == 'push_back':
+        q.extend(i)
+    elif act == 'pop_front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif act == 'pop_back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.pop())
+    elif act == 'size':
+        print(len(q))
+    elif act == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif act == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif act == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
