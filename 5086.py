27db36cd5aac35d1cb9e0c95eb73256f4e91d1e6
jrary <jrary@naver.com>
2023-06-17 23:53:48 +0900

    23/06/17 - #5086

---
+++ 5086.py
@@ -0,0 +1,11 @@
+import sys
+while True :
+    x, y = map(int, sys.stdin.readline().split())
+    if x == 0 or y == 0:
+        break
+    if y%x == 0:
+        print('factor')
+    elif x%y == 0:
+        print('multiple')
+    else:
+        print('neither')
