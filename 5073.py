150ff5c3519d29dd60c618db102f8eac059cd00c
jrary <jrary@naver.com>
2023-07-06 00:05:35 +0900

    23/07/05 - #5073

--- 5073.py
+++ 5073.py
@@ -4,10 +4,18 @@ while True:
     a, b, c = map(int, sys.stdin.readline().split())
     if a == 0:
         break
+    if a > b:
+        a, b = b, a
+    if b > c:
+        b, c = c, b
+    if a > b :
+        a, b = b, a
 
-    if a == b and b == c:
+    if c >= a + b:
+        res.append('Invalid')
+    elif a == b and b == c:
         res.append('Equilateral')
-    elif a == b or b == c or a == c:
+    elif a == b or b == c:
         res.append('Isosceles')
     else:
         res.append('Scalene')
