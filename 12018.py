2ce6863b3ce5fa74a74d2c045d1c77e7bce6f2da
jrary <jrary@naver.com>
2023-07-09 23:56:54 +0900

    23/07/09 - #12018

--- 12018.py
+++ 12018.py
@@ -5,12 +5,19 @@ for _ in range(N):
     P, L = map(int, sys.stdin.readline().split())
     arr = list(map(int, sys.stdin.readline().split()))
     arr.sort(reverse=True)
-    print(L-1)
-    milage.append(arr[L-1]-1)
+    if len(arr) < L:
+        milage.append(1)
+    elif L != 1 and arr[L-2] == arr[L-3]:
+        milage.append(arr[L-2])
+    elif L == 1 and arr[0] == 36:
+        milage.append(36)
+    else:
+        milage.append(arr[L-1]+1)
+print(milage)
 milage.sort()
 now_milage = 0
 for i in range(N):
-    now_milage += milage
+    now_milage += milage[i]
     if now_milage > M:
-        print(i+1)
+        print(i)
         break
