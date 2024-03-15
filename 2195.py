3c493fe9b04a532c9209f7eb4e8524531051c794
jrary <jrary@naver.com>
2024-03-15 10:00:00 +0900

    Date:      Fri Mar 15 10:00:00 2024 +0900

--- 2195.py
+++ 2195.py
@@ -3,19 +3,26 @@ import sys
 S = sys.stdin.readline().strip()
 P = sys.stdin.readline().strip()
 P_split = []
+
+# copy 함수를 통한 결과물을 모두 미리 가져온다
+# S를 split하여 copy 함수 안에 문자열의 길이별로 저장한다
 copy = [[] for _ in range(len(S)+1)]
 for i in range(len(S)):
     for j in range(i+1, len(S)+1):
         copy[j-i].append(S[i:j])
 
 def frag(p):
-    
+    print("hi")
 
 res = 0
-while len(P_split) > 0:
+if len(S) < len(P):
     for i in range(len(P)):
         for j in range(i, len(P)):
-            
+            P_split.append(P[i:j])
+else:
+    P_split = P
+
+print(P_split)
 
 
 print(res)
