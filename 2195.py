d482c2d54927cd9f96b15078cfe28465335381a1
jrary <jrary@naver.com>
2024-03-17 10:00:00 +0900

    24/03/17 - #2195 - 시간초과

--- 2195.py
+++ 2195.py
@@ -10,23 +10,22 @@ copy = [[] for _ in range(len(S)+1)]
 for i in range(len(S)):
     for j in range(i+1, len(S)+1):
         copy[j-i].append(S[i:j])
+
+# 가장 긴 특정 문자열이 매칭됐다면 해당 문자열을 제외한 앞, 뒤 문자열에 대하여 다시 매칭을 진행
 res = 0
 def frag(p):
     global res
     if len(p) <= 1:
         res += 1
-        #print(p)
         return
     for i in range(len(S), 1, -1):
         for key in copy[i]:
             if key in p:
                 idx = p.find(key)
-                #print(key,":", p, "->", p[:idx], "+", p[idx+i:])
                 res += 1
                 frag(p[:idx])
                 frag(p[idx+i:])
                 return          
 
 frag(P)
-# print(copy)
 print(res)
